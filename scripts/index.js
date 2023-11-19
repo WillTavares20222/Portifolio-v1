c2e6c2ce98001d39342a092c2f4ddea0236d4e27
Willian tavares <willianmatheustav@gmail.com>
2023-11-19 11:16:07 -0400

    Contador de visita adicionado

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@ const navbar = document.querySelector('.navbar');
 const mobileNavbar = document.querySelector('.navbar__mobile');
 const button = document.querySelector('.burguer');
 
+
 button.addEventListener('click', function () {
   mobileNavbar.classList.toggle('active');
 });
